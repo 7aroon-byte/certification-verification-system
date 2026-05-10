c60a8f771e521dac6ef96ca39a5656c25d14edb2
7aroon-byte <s7afiu7aroon@gmail.com>
2026-05-10 23:37:31 +0800

    hardhat update

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -14,5 +14,9 @@ module.exports = {
     localhost: {
       url: process.env.LOCAL_RPC_URL || "http://127.0.0.1:8545"
     },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/0lOeq1KY_Ab_V2avqupdm",
+      accounts: ["1f13575d5045dbed79126f54e395666651eaff87cd2612363f9885cedd0c52a1"]
+    }
   },
 };
